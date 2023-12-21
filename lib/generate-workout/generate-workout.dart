7bcd0bc5e604dd5b48d65cf90adb43336775539a
benjamin32561 <ben32561@gmail.com>
2023-12-21 12:55:36 +0200

    cool

---
+++ lib/generate-workout/generate-workout.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// a simple statefull widget named "GenerateWorkoutWidget"
+class GenerateWorkoutWidget extends StatefulWidget {
+  const GenerateWorkoutWidget({Key? key}) : super(key: key);
+
+  @override
+  _GenerateWorkoutWidgetState createState() => _GenerateWorkoutWidgetState();
+}
+
+// the state of the widget
+class _GenerateWorkoutWidgetState extends State<GenerateWorkoutWidget>{
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
